56d67895de7c2b077dbb19d5a1c2aa4e11771543
✰ 𝙱𝙻𝙰𝙲𝙺 𝙹𝙰𝙲𝙺 么 ✰ <lawrenceraissebs@gmail.com>
2025-01-10 13:21:47 +0200

    Primeira versão

---
+++ Pirâmide.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Eu amo essa palavra, "void"
+void repetidor(char *txt, int vezes) {
+    int c4; // É isso aí, C4
+    for (c4 = 1; c4 <= vezes; c4++) {
+        printf("%s ", txt);
+    }
+}
+
+int main() {
+    int numero, c, cont = 0;
+    
+    // É, usei o "c" porque o "i" precisava descansar
+    printf("Digite um número inteiro entre 0 e 8: ");
+    scanf("%d", &numero);
+
+    for (c = 0; c <= numero; c++) {
+        cont++;
+        repetidor("*", cont);
+        printf("\n");
+    }
+
+    return 0;
+}
